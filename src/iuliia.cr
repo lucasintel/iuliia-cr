9904751e6375d0f02ff27623927e4d6b37ecde50
kandayo <kdy@absolab.xyz>
2022-01-09 20:39:43 -0300

    docs: docs improvaments

--- src/iuliia.cr
+++ src/iuliia.cr
@@ -1,7 +1,25 @@
 require "./iuliia/schemas/*"
 
 module Iuliia
+  # Returns the available transliteration standards.
+  #
+  # For more information on the schemas, see https://dangry.ru/iuliia
+  #
+  # ```
+  # schema = Iuliia.available_schemas.first
+  # # => Iuliia::Mosmetro
+  #
+  # schema.description
+  # # => "Moscow Metro map transliteration schema"
+  #
+  # schema.name
+  # # => "mosmetro"
+  #
+  # Iuliia.available_schemas.each do |schema|
+  #   schema.translate("Битцевский Парк")
+  # end
+  # ```
   def self.available_schemas
-    {{ Iuliia::Engine.includers.map(&.instance) }}
+    {{ Iuliia::Engine.includers }}
   end
 end
